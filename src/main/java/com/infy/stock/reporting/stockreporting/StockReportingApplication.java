da8e5de4db4a70975b229edd72dde0d6a9d95024
Alisha Chowdhury <alisha.chowdhury01@ad.infosys.com>
2023-09-21 21:56:44 +0530

    initial project structure creation

---
+++ src/main/java/com/infy/stock/reporting/stockreporting/StockReportingApplication.java
@@ -0,0 +1,13 @@
+package com.infy.stock.reporting.stockreporting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockReportingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockReportingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/infy/stock/reporting/stockreporting/StockReportingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infy.stock.reporting.stockreporting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockReportingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
